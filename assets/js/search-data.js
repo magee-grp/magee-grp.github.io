f23c48e2bed7212572321971048e6f54b73d99e9
afmagee <andyfmagee@gmail.com>
2026-04-14 05:02:24 +0000

    Deploying to gh-pages from @ magee-grp/magee-grp.github.io@ddd8424a8f78b0ac9739c2ac51197b287a9d166a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+        },{id: "nav-research",
+          title: "research",
+          description: "Our research.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/research/";
           },
         },{id: "nav-repositories",
           title: "repositories",
